d7aeae910e4ada4efd1b97e055cd6b6773ac11d2
rockske8er <rockske8er@gmail.com>
2023-05-15 19:51:04 -0300

    feat((components)):create Work experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from './components/pages/home/Hero'
 import { HighlightProjects } from './components/pages/home/HighlightProjects'
 import { KnowTechs } from './components/pages/home/KnowTechs'
+import { WorkExperience } from './components/pages/home/WorkExperience'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <KnowTechs />
       <HighlightProjects />
+      <WorkExperience />
     </>
   )
 }
