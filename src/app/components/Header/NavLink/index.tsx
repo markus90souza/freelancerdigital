11b169c6146d925e8d6a3a85e757a4a715675348
rockske8er <rockske8er@gmail.com>
2023-05-14 23:27:14 -0300

    feat((components)):create header/navlink

---
+++ src/app/components/Header/NavLink/index.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/app/libs/utils'
+
+interface NavLinkProps {
+  label: string
+  href: string
+}
+
+export const NavLink = ({ href, label }: NavLinkProps) => {
+  const pathname = usePathname()
+
+  const is_active = pathname === href
+  return (
+    <Link
+      href={href}
+      className={cn(
+        'flex items-center gap-2 text-gray-400 font-mono font-medium',
+        is_active && 'text-emerald-500',
+      )}
+    >
+      <span className="text-emerald-400">#</span>
+      {label}
+    </Link>
+  )
+}
---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { NavLink } from './NavLink'
+
+export const Header = () => {
+  const NAVLINKS = [
+    { label: 'Home', href: '/' },
+    { label: 'Projetos', href: '/projects' },
+  ]
+
+  return (
+    <header className="absolute top-0 w-full h-24 z-10 flex items-center justify-center">
+      <div className="container flex items-center justify-between">
+        <Link href={'/'}>
+          <Image
+            src={'/next.svg'}
+            width={50}
+            height={50}
+            alt={'frelancerdigital'}
+          />
+        </Link>
+
+        <nav className="flex items-center gap-4">
+          {NAVLINKS.map((item) => (
+            <NavLink {...item} key={item.href} />
+          ))}
+        </nav>
+      </div>
+    </header>
+  )
+}
