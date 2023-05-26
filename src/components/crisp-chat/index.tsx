d0eee982e45dd43f7353a32a44d6ea96e9fc7919
rockske8er <rockske8er@gmail.com>
2023-05-26 17:30:04 -0300

    feat((web)):custom crisp chat

--- src/components/crisp-chat/index.tsx
+++ src/components/crisp-chat/index.tsx
@@ -1,10 +1,14 @@
 'use client'
 
-import { Crisp } from 'crisp-sdk-web'
+import { ChatboxColors, Crisp } from 'crisp-sdk-web'
 import { useEffect } from 'react'
 export function CrispChat() {
   useEffect(() => {
-    Crisp.configure('a4efb6f3-1b54-4d30-a983-e2171e03f090')
+    Crisp.configure('a4efb6f3-1b54-4d30-a983-e2171e03f090', {
+      autoload: true,
+      locale: 'pt-BR',
+    })
+    Crisp.setColorTheme(ChatboxColors.Black)
   })
   return null
 }
